c1d743ce5c2d77b8d50948c40e6d37b48ad9517f
Yao Qiaori <yaoqr15@lzu.edu.cn>
2017-11-29 18:59:19 +0800

    finish the entire login && register

--- src/components/App.vue
+++ src/components/App.vue
@@ -6,7 +6,7 @@
     
     <div class="block1">
       <el-row :gutter="20" class="body-style">
-            <span class="demonstration" v-model="cha1">这里你将见到知识的海洋</span>
+            <span class="demonstration">这里你将见到知识的海洋</span>
       </el-row>
       <el-row :gutter="20">
         <el-col :span="10">
@@ -14,24 +14,25 @@
         </el-col>
         <el-col :span="8" class="col-style">
           <div class="tab1">
-            <el-tabs v-model="activeName" @tab-click="handleClick" class="tab-size">
+            <el-tabs v-model="activeName" class="tab-size">
               <el-tab-pane name="first"></el-tab-pane>
+
               <el-tab-pane label="登录" name="second">
-                <div class="input3">
-                  <el-input v-model="username" placeholder="手机号或邮箱" :cols="3" class="input-style"
-                    maxlength=64 minlength=6 ></el-input>
-                </div>
-                <div class="input4">
-                  <el-input v-model="password" placeholder="密码" :cols="3" class="input-style"
-                    maxlength=64 minlength=6 type="password"></el-input>
-                </div>
-                <div class="button1">
-                  <el-button class="el-button" @click="loginSubmit(username,password)" type="primary">登录</el-button> 
+                <el-form :model="loginForm" status-icon :rules="loginRules" ref="loginForm">
+                  <el-form-item prop="loginUsername" class="input-style">
+                    <el-input v-model="loginForm.loginUsername" placeholder="手机号/邮箱" ></el-input>
+                  </el-form-item>
+                  <el-form-item prop="loginPass" class="input-style">
+                    <el-input type="password" placeholder="密码" v-model="loginForm.loginPass" auto-complete="off"></el-input>
+                  </el-form-item>
+                </el-form>
+                <div>
+                  <el-button class="el-button" @click="loginSubmit(loginForm.loginUsername,loginForm.loginPass)" type="primary">登录</el-button> 
                 </div>
                 <el-row>
                   <el-col :span="8">
-                    <div class="button2">
-                      <el-button type="text" v-model="button2" class="text-button-style1">手机二维码登录</el-button>
+                    <div>
+                      <el-button type="text" class="text-button-style1">手机二维码登录</el-button>
                     </div>
                   </el-col>
                   <el-col :span="1">
@@ -39,37 +40,37 @@
                     </div>
                   </el-col>
                   <el-col :span="1">
-                    <div class="button3">
-                      <el-button type="text" v-model="button3" class="text-button-style2" >无法登录?</el-button>
+                    <div>
+                      <el-button type="text" class="text-button-style2" >无法登录?</el-button>
                     </div>
                   </el-col>
                 </el-row>
-                {{flag}}
               </el-tab-pane>
+              
               <el-tab-pane name="third"></el-tab-pane>
+
               <el-tab-pane name="fourth" label="注册">
                 <!--这里就需要稍微布局一下了-->
-                <div class="input5">
-                  <el-input v-model="input5" placeholder="姓名" :cols="3" class="input-style"></el-input>
-                </div>
-                <div class="input6">
-                  <el-input v-model="input6" placeholder="手机号" :cols="3" class="input-style"></el-input>
-                </div>
-                <div class="input7">
-                  <el-input v-model="input7" placeholder="邮箱号" :cols="3" class="input-style"></el-input>
-                </div>
-                <div class="input8">
-                  <el-input v-model="input8" placeholder="密码" :cols="3" class="input-style"></el-input>
-                </div>
-                <div class="input9">
-                  <el-input v-model="input9" placeholder="密码确认" :cols="3" class="input-style"></el-input>
-                </div>
-                <div class="input10">
-                  <el-input v-model="input10" placeholder="身份证号" :cols="3" class="input-style"></el-input>
-                </div>
-                <div class="button3">
-                  <el-button class="el-button" v-model="button1" type="primary">注册</el-button>
-                </div>
+
+                <el-form :model="regForm" status-icon :rules="regRules" ref="regForm">
+                  <el-form-item prop="regUsername" class="input-style">
+                    <el-input v-model="regForm.regUsername" placeholder="手机号/邮箱" ></el-input>
+                  </el-form-item>
+                  <el-form-item prop="regPass" class="input-style">
+                    <el-input type="password" placeholder="密码" v-model="regForm.regPass" auto-complete="off"></el-input>
+                  </el-form-item>
+                  <el-form-item prop="checkPass" class="input-style">
+                    <el-input type="password" placeholder="密码确认" v-model="regForm.checkPass" auto-complete="off"></el-input>
+                  </el-form-item>
+                  
+                  <el-form-item>
+                    <el-button type="primary" @click="regSubmit('regForm')" class="reg-button">提交</el-button>
+                    <div>
+                      <el-button @click="resetForm('regForm')" class="reg-button">重置</el-button>
+                    </div>
+                  </el-form-item>
+                </el-form>
+
               </el-tab-pane>
             </el-tabs>
           </div>
@@ -82,60 +83,121 @@
 <script>
 export default {
   data (){
+    //  element-ui中可以自定义表单的验证方法
+    //  检查用户名
+    var checkUsername = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('账户不能为空'));
+        }
+        else {
+          callback();
+        }
+      };
+    //  检查注册密码是否为空 并调用重复密码的检测
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'));
+      } else {
+        if (this.regForm.checkPass !== '') {
+          this.$refs.regForm.validateField('checkPass');
+        }
+        callback();
+      }
+    };
+    //  检查注册的重复密码
+    var validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'));
+      } else if (value !== this.regForm.regPass) {
+        callback(new Error('两次输入密码不一致!'));
+      } else {
+        callback();
+      }
+    };
+    //  检查登陆的密码是否为空
+    var LoginValidatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'));
+      } else {
+        callback();
+      }
+    };
+    //  我们约定俗成的账户状态码
+    var account_code = {
+          "100": "not_POST",
+          "101": "username_invalid",
+          "102": "password_invalid",
+          "103": "username_existed",  //for register
+          "105": "username_not_exist",  //for login
+          "106": "password_error",
+          "110": "register_success",
+          "120": "login_success",
+          "130": "reset_password_success",
+          "140": "logout_success",
+          "150": "options_request"
+    };
+
+    //  在子Vue实例中使用到的成员属性和方法等
     return {
-			input: '',
-    	activeName: 'second',
-      username: '',
-      password: '',
-      flag: ''
-    }
+      activeName: 'second',
+      account_code,
+      //  登录中使用的属性
+			loginForm: {
+          loginPass: '',
+          loginUsername: ''
+      },
+      //  登陆表单的验证规则
+      loginRules: {
+        loginPass: [
+          { validator: LoginValidatePass, trigger: 'blur' }
+        ],
+        loginUsername: [
+          { validator: checkUsername, trigger: 'blur' }
+        ]
+      },
+      //  注册中使用的属性
+      regForm: {
+          regPass: '',
+          checkPass: '',
+          regUsername: ''
+      },
+      //  注册表单的验证规则
+      regRules: {
+        regPass: [
+          { validator: validatePass, trigger: 'blur' }
+        ],
+        checkPass: [
+          { validator: validatePass2, trigger: 'blur' }
+        ],
+        regUsername: [
+          { validator: checkUsername, trigger: 'blur' }
+        ]
+      }
+      };
   },
   methods: {
-    startHacking () {
-      this.$notify({
-        title: 'It Works',
-        message: 'We have laid the groundwork for you. Now it\'s your time to build something epic!',
-        duration: 6000
-      })
-    },
-    handleSelect (key, keyPath) {
-      console.log(key, keyPath)
-    },
+    //  实现登陆按钮的交互功能 && 与后端的数据交换
     loginSubmit (username, password) {
       if(username.length>5 && username.length<64) {
           if(password.length>5 && password.length<64) {
-            var account_code = {
-            "100": "not_POST",
-            "101": "username_invalid",
-            "102": "password_invalid",
-            "103": "username_existed",  //for register
-            "105": "username_not_exist",  //for login
-            "106": "password_error",
-            "110": "register_success",
-            "120": "login_success",
-            "130": "reset_password_success",
-            "140": "logout_success",
-            "150": "options_request"
-        };
-        let temp = {
-            "username": username,
-            "password": password
-        };
-        let account = JSON.stringify(temp);
-        this.$http.post("http://127.0.0.1:8000/login/",account)
-                  .then((response) => {
-                      console.log("OK");
-                      console.log(response);
-                      this.flag = "OJBK!!";
-                      alert(account_code[response.body]);
-                      if(response.body == "120") {
-                        this.$router.push('/hello');
-                      }
-                  },
-                  (response) => {
-                         console.log("Not OK");
-                         console.log(response);
-                  });
+            let temp = {
+                "username": username,
+                "password": password
+          };
+          let account = JSON.stringify(temp);
+          this.$http.post("http://127.0.0.1:8000/login/",account)
+                    .then((response) => {
+                        console.log("OK in login");
+                        console.log(response);
+                        alert(this.account_code[response.body]);
+                        if(response.body == "120") {
+                          this.$router.push('/hello');
+                        }
+                    },
+                    (response) => {
+                          console.log("Not OK in login");
+                          console.log(response);
+                    });
           }
           else {
             alert("Your password input is invalid !!");
@@ -143,12 +205,58 @@ export default {
       }
       else {
         alert("Your username input is invalid !!");
+      } 
+    },
+    //  实现注册按钮的交互功能 && 与后端的数据交换
+    regSubmit(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            let username = this.regForm.regUsername,
+                password = this.regForm.regPass;
+            if(username.length>5 && username.length<64) {
+                if(password.length>5 && password.length<64) {
+                  let temp = {
+                      "username": username,
+                      "password": password
+                };
+                let account = JSON.stringify(temp);
+                this.$http.post("http://127.0.0.1:8000/register/",account)
+                          .then((response) => {
+                              console.log("register OK");
+                              console.log(response);
+                              alert(this.account_code[response.body]);
+                              if(response.body == "110") {
+                                this.$router.push('/hello');
+                              }
+                          },
+                          (response) => {
+                                console.log("Not OK in register");
+                                console.log(response);
+                          });
+                }
+                else {
+                  alert("Your password input is invalid !!");
+                }
+            }
+            else {
+              alert("Your username input is invalid !!");
+            }
+            return true;
+          } 
+          else {
+            console.log('error register submit!!');
+            return false;
+          }
+        });
+      },
+      //  重置按钮的功能实现
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
       }
-        
-    }
   }
 }
 </script>
+
 <style>
 body {
   font-family: Helvetica, sans-serif,MicrosoftYahei;
@@ -200,7 +308,7 @@ body {
     padding-left: 15px;
   }
   .input-style {
-    margin-bottom: 5px;
+    margin-bottom: 17px;
     width: 230px;
     height: 40px;
   }
@@ -213,6 +321,7 @@ body {
     width:135px;
     color:grey;
   }
-    
-  
+  .reg-button {
+    margin-bottom: 5px;
+  }
 </style>
